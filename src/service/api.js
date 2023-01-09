b628b0f30d308c09eebe048004283d22a76436c1
IMorv <109215930+IMorv@users.noreply.github.com>
2023-01-09 13:06:27 -0300

    Consegui arrumar o problema

---
+++ src/service/api.js
@@ -0,0 +1,12 @@
+const KEY = '1192354c43aa45aaa55123432230401';
+
+const fetchData = async (city) => {
+  const url = `http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no`;
+
+  const fetchResponse = await fetch(url);
+  const data = await fetchResponse.json();
+
+  return data;
+};
+
+export default fetchData;
